4dbf92b4e3d4551119620d806fa29a6346f6fefd
Bojan Vrhovnik <bojan@vrhovnik.net>
2020-11-10 12:03:08 +0100

    adding console application for testing out the API available on different addresses

---
+++ src/VMSLN/VM.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VM.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Local Console API to test out the connectivity between migration of the VM");
+
+            var url = Environment.GetEnvironmentVariable("TEST_URL"); 
+            
+            //initial call from local machine to test out the API
+            var client = new HttpClient();
+            while (true) //TODO: background process for checking out functionality
+            {
+                var receivedInput = await client.GetStringAsync(url);
+                System.Console.WriteLine(receivedInput);
+                await Task.Delay(2000); //delay for 2 seconds and call again
+            }
+        }
+    }
+}
